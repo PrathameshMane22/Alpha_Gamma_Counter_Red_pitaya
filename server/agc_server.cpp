43dd068a3b991a53436c838af366853aca3eee17
PrathameshMane22 <118462896+PrathameshMane22@users.noreply.github.com>
2025-06-30 10:37:23 +0530

    Update agc_server.cpp

--- server/agc_server.cpp
+++ server/agc_server.cpp
@@ -1,10 +1,22 @@
 /*
-    Alpha Gamma Counter with TCP Streaming to PC
-    Streams unified CSV data: time_alpha,amp_alpha,time_gamma,amp_gamma
-    Original functionality fully preserved, only unified CSV now streams to PC
+    Alpha Gamma Counter
+    Copyright (C) 2025 TIFR
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define VERSION "1.6"
+#define VERSION "1.5"
 
 #include <cstdio>
 #include <cstdlib>
